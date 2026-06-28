e766588d638a290188586a7e15c9a5d2504efc10
tomyroko <tomyroko@gmail.com>
2026-06-28 23:34:32 +0200

    .

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,13 +1,18 @@
-import User from "../models/user.model.js";
+import User from "../models/User.js";
 
 export const adminMiddleware = async (req, res, next) => {
   try {
+    const userId = req.user?.id;
+    if (!userId) {
+      return res.status(401).json({ message: "Usuario no autenticado" });
+    }
+
     const user = await User.findById(userId);
-    if (!user || !user.isAdmin) {
+    if (!user || !user.admin) {
       return res.status(403).json({ message: "Acceso denegado" });
     }
     next();
   } catch (error) {
     return res.status(500).json({ message: "Error del servidor" });
   }
-}
+};
