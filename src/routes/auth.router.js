55a172a3d6417888d1b2f4a5e8f07648dd2580e7
tomyroko <tomyroko@gmail.com>
2026-06-28 20:47:25 +0200

    .

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -5,8 +5,6 @@ const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-  // Aquí iría la lógica de inicio de sesión
-  res.json({ message: "Inicio de sesión exitoso" });
-});
+
 
 export default router;
